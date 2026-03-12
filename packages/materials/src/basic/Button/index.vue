dc33b08e1d0c58a76fd1890e8eee6e764eec7692
wangmaoquan <wangmaoquan@example.com>
2026-03-12 12:08:58 +0800

    fix: 修复 Button 组件 - 添加默认值、修复 SVG path typo

--- packages/materials/src/basic/Button/index.vue
+++ packages/materials/src/basic/Button/index.vue
@@ -29,6 +29,8 @@ const props = withDefaults(defineProps<{
   loading: false,
   block: false,
   shape: 'default',
+  icon: '',
+  iconRight: '',
   htmlType: 'button',
 })
 
@@ -128,7 +130,7 @@ function handleClick(event: MouseEvent) {
       stroke-width="2"
     >
       <path v-if="icon === 'plus'" stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
-      <path v-else-if="icon === 'search'" stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 0 011 7 74 0z" />
+      <path v-else-if="icon === 'search'" stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
       <path v-else-if="icon === 'edit'" stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
       <path v-else-if="icon === 'delete'" stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
       <path v-else-if="icon === 'download'" stroke-linecap="round" stroke-linejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
